4e1d76330c4881f2968be3a5ddf5fb25864e0f8b
Zenil001 <109790442+Zenil001@users.noreply.github.com>
2022-07-29 19:40:09 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,22 @@
+function alertFun(){
+    alert('Hello Tarannum');
+}
+
+function confirmFun(){
+    if(confirm("Are you sure..??"))
+    {
+        alert("YESSSSS");
+    }
+    else
+    {
+        alert("NOOOOO");
+    }
+}
+
+function promptFun(){
+    var FirstName=prompt("Enter First Name Here..");
+    var MiddleName=prompt("Enter Middle Name Here..");
+    var LastName=prompt("Enter Last Name Here..");
+    alert(FirstName +" "+MiddleName+" "+LastName);
+}
+
